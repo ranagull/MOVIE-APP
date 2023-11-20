58cffffa43e796257f58a3e190b484e696fd2042
ranagull <ranagul1236@gmail.com>
2023-11-20 12:10:11 +0300

    tailwind css added

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text className="text-red-500 text-4xl">Rana's First React Native Application!</Text>
       <StatusBar style="auto" />
     </View>
   );
